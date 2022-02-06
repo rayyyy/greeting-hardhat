f29c174af17dd6090521a5cb65e967cb37f6f942
rayyyy <nraavy@gmail.com>
2022-02-06 22:59:48 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['standard', 'prettier', 'plugin:node/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      { ignores: ['modules'] },
+    ],
+    indent: ['error', 2],
+    quotes: ['error', 'single', 'avoid-escape'],
+    semi: ['error', 'never'],
+  },
+}
